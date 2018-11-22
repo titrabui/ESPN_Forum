11811a4d57ba4ecaba3fbf0bebe63a7108302c11
titrabui <titrabui@gmail.com>
2018-11-23 02:17:21 +0700

    add users model and authentication devise

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,19 +2,18 @@ class MessagesController < ApplicationController
   before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
-    @message = Message.all.order('created_at DESC')
+    @messages = Message.all.order('created_at DESC')
   end
 
   def show
   end
 
   def new
-    @message = Message.new
+    @message = current_user.messages.build
   end
 
   def create
-    @message = Message.new(message_params)
-
+    @message = current_user.messages.build(message_params)
     if @message.save
       redirect_to root_path
     else
